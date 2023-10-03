08b8c6fa88e284738a64e326eea67b56b8d7173f
IrinaVolodko <8232734@gmail.com>
2023-10-03 14:33:19 +0300

    Second task with tables

--- Lection_1_in(out)put/main.py
+++ Lection_1_in(out)put/main.py
@@ -1,8 +1,26 @@
 #За день машина проезжает n километров. Сколько дней нужно, чтобы проехать маршрут
 #длиной m километров? При решении этой задачи нельзя пользоваться условной конструкцией if и циклами.
-import math
 
-n = int(input("Input speed of car: "))
-m = int(input("Input destination: "))
-days = math.ceil(m/n)
-print (f"{days} days for this destination")
+
+# from math import ceil 
+
+# speed = int(input("Input speed of car: "))
+# distance = int(input("Input distance: "))
+# days = ceil(distance/speed)
+# print (f"{days} days for this destination")
+
+#В некоторой школе решили набрать три новых математических класса и оборудовать кабинеты для них 
+#новыми партами. За каждой партой может сидеть два учащихся. Известно количество учащихся в каждом из 
+#трех классов. Выведите наименьшее число парт, которое нужно приобрести для них.
+
+from math import ceil 
+
+first_class_pupils = int(input("Pupils of first class: "))
+second_class_pupils = int(input("Pupils of second class: "))
+third_class_pupils = int(input("Pupils of third class: "))
+
+table1 = ceil(first_class_pupils/2);
+table2 = ceil(second_class_pupils/2)
+table3 = ceil(third_class_pupils/2)
+
+print (f'Sum of tables for 3 classes: {table1+table2+table3}')
