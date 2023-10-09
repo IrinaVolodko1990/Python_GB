269d0f9861c491b895fe8882ae1bb75e67164518
IrinaVolodko <8232734@gmail.com>
2023-10-09 10:47:34 +0300

    3th task in second lection

--- Lection_2 _while(for)/main.py
+++ Lection_2 _while(for)/main.py
@@ -14,17 +14,36 @@
 # Ряд чисел Фибоначчи:
 # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, …
 
-number = int (input("Input some number > 1: "))
-first_F_number = 0
-second_F_number = 1
-index = 1
-temp = 0
+# number = int (input("Input some number > 1: "))
+# first_F_number = 0
+# second_F_number = 1
+# index = 1
+# temp = 0
 
-while second_F_number <= number:
-    temp = first_F_number + second_F_number
-    first_F_number = second_F_number
-    second_F_number = temp
-    index += 1
-print (index)
+# while second_F_number <= number:
+#     temp = first_F_number + second_F_number
+#     first_F_number = second_F_number
+#     second_F_number = temp
+#     index += 1
+# print (index)
 
+# Пользователь вводит число N (1 ≤ N ≤ 10). Далее построчно N чисел от -50 до 50. 
+# Нужно вывести наибольшее количество идущих подряд положительных чисел.
+
+import random
+
+days = int(input("Input qty of days: "))
+count = 0
+max_pos_num = 0
+for i in range (1, days):
+    num = random.randint(-50, 50)
+    print (num)
+    if num > 0:
+        count += 1
+        if max_pos_num < count:
+            max_pos_num = count
+    else:
+        count = 0
+print (f"{max_pos_num} days in a row ")
+    
 
