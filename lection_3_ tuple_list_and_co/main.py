233bd5e8ea47f8e40af3a55a988a36e17ef2e685
IrinaVolodko <8232734@gmail.com>
2023-10-23 14:39:18 +0300

    Task with set in 3th sem

--- lection_3_ tuple_list_and_co/main.py
+++ lection_3_ tuple_list_and_co/main.py
@@ -21,24 +21,64 @@
 
 # Output: [3, 4, 5, 1, 2]
 
-import random
+# import random
 
 # initial_list = [1, 2, 3, 4, 5]
-number = int(input('Input k for replace: '))
-size = random.randint(0,9)
-initial_list = [random.randint(-100,100) for i in range (size)]
-new_list = [] 
-print(initial_list)
-
-if number >= len(initial_list):
-    print('Impossible for replace')
-else:
-    for index in range(size):
-        if index >= number - 1:
-            new_list.append(initial_list[index])
-    for index in range (number - 1): # не работает при number = 1
-        new_list.append(initial_list[index])
+# number = int(input('Input k for replace: '))
+# size = random.randint(0,9)
+# initial_list = [random.randint(-100,100) for i in range (size)]
+# new_list = [] 
+# print(initial_list)
+# # INCORRECT
+# # if number >= len(initial_list):
+# #     print('Impossible for replace')
+# # else:
+# #     for index in range(size):
+# #         if index >= number - 1:
+# #             new_list.append(initial_list[index])
+# #     for index in range (number - 1): # не работает при number = 1
+# #         new_list.append(initial_list[index])
        
-print(new_list)
+# for _ in range(number): 
+#     last_number = initial_list.pop()
+#     initial_list.insert(0, last_number)
+     
+# print(new_list)
 # print ((initial_list[-number:] + initial_list[: -number]))
 
+
+
+# Напишите программу для печати всех уникальных значений в словаре.
+# Input: [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, 
+#         {"VI": "S005"}, {"VII": "S005"}, {"V":"S009"}, {"VIII":"S007"}]
+
+
+# d = [{"V": "S001"}, 
+#      {"V": "S002"}, 
+#      {"VI": "S001", "VIIII":"S00007"}, 
+#      {"VI": "S005"}, 
+#      {"VII": "S005"}, 
+#      {"V":"S009"}, 
+#      {"VIII":"S007"}]
+
+# values_set = set()
+# for item in d:
+#     values_set.update(item.values())
+
+#     # * - распаковка объекта, но метод add не работает при наличии в словаре нескольких пар ключ-значение
+#     # values_set.add(*item.values()) 
+
+
+#     # код для работы с объединением union
+#     values_set |= set(item.values())
+
+    
+# print (values_set)
+
+# Дан массив, состоящий из целых чисел. Напишите программу, которая подсчитает 
+# количество элементов массива, больших предыдущего (элемента с предыдущим номером)
+
+# Input: [0, -1, 5, 2, 3]
+# Output: 2
+# Пояснение: (-1 < 5, 2 < 3)
+# Примечание: Пользователь может вводить значения списка или список задан изначально.
