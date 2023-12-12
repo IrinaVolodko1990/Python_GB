454ce457fd232792e995723bcd75fbe447f637d6
IrinaVolodko <8232734@gmail.com>
2023-12-12 20:00:16 +0300

    Update HW3

--- HW_3/main.py
+++ HW_3/main.py
@@ -68,43 +68,61 @@
 # которое содержит либо только английские, либо только русские буквы.
 
 
-letters_dict = {
-    (
-        "A",
-        "E",
-        "I",
-        "O",
-        "U",
-        "L",
-        "N",
-        "S",
-        "T",
-        "R",
-        "А",
-        "В",
-        "Е",
-        "И",
-        "Н",
-        "О",
-        "Р",
-        "С",
-        "Т",
-    ): 1,
-    ("D", "G", "Д", "К", "Л", "М", "П", "У"): 2,
-    ("B", "C", "M", "P", "Б", "Г", "Ё", "Ь", "Я"): 3,
-    ("F", "H", "V", "W", "Y", "Й", "Ы"): 4,
-    ("K", "Ж", "З", "Х", "Ц", "Ч"): 5,
-    ("J", "X", "Ш", "Э", "Ю"): 8,
-    ("Q", "Z", "Ф", "Щ", "Ъ"): 10,
-}
+# letters_dict = {
+#     (
+#         "A",
+#         "E",
+#         "I",
+#         "O",
+#         "U",
+#         "L",
+#         "N",
+#         "S",
+#         "T",
+#         "R",
+#         "А",
+#         "В",
+#         "Е",
+#         "И",
+#         "Н",
+#         "О",
+#         "Р",
+#         "С",
+#         "Т",
+#     ): 1,
+#     ("D", "G", "Д", "К", "Л", "М", "П", "У"): 2,
+#     ("B", "C", "M", "P", "Б", "Г", "Ё", "Ь", "Я"): 3,
+#     ("F", "H", "V", "W", "Y", "Й", "Ы"): 4,
+#     ("K", "Ж", "З", "Х", "Ц", "Ч"): 5,
+#     ("J", "X", "Ш", "Э", "Ю"): 8,
+#     ("Q", "Z", "Ф", "Щ", "Ъ"): 10,
+# }
 
-str_1 = str(input("Input some string: "))
-sum = 0
+# str_1 = str(input("Input some string: "))
+# sum = 0
 
-for key, value in letters_dict.items():
-    for letter_index in range(len(str_1)):
-        for letter in range(len(key)):
-            if str_1[letter_index].upper() == key[letter]:
-                sum += value
+# for key, value in letters_dict.items():
+#     for letter_index in range(len(str_1)):
+#         for letter in range(len(key)):
+#             if str_1[letter_index].upper() == key[letter]:
+#                 sum += value
 
-print(sum)
+# print(sum)
+
+# 2nd option
+
+points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
+points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+k = 'ноутбук'
+word = k.upper()  # переводим все буквы в верхний регистр
+count = 0
+for i in word:
+    if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+        for j in points_en:
+            if i in points_en[j]:
+                count = count + j
+    else:
+        for j in points_ru:
+            if i in points_ru[j]:
+                count = count + j
+print(count)
